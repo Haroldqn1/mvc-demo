d03845f3271e442fda14f8190cfcf360ab024254
Jesús Raúl <1269884@senati.pe>
2023-04-03 13:54:13 -0500

    Actualizacion con AJAX

--- controllers/curso.controller.php
+++ controllers/curso.controller.php
@@ -1,22 +1,25 @@
 <?php
-
 // INCORPORAR AL MODELO
 require_once "../models/curso.php";
 
 //Verificar si existe un operacion en curso
 //GET - POST - REQUEST
 //'isset' determina si existe un objeto
-if(isset($_POST['operacion'])){
+if (isset($_POST['operacion'])){
 
   //Instancia de la clase curso
   $curso = new Curso();
 
   //Identificar la operacion: Listar , insertar , eliminar, bucar , etc.
-  if($_POST['operacion'] == 'listar'){
+  if ($_POST['operacion'] == 'listar'){
     //Utilizamos el metodo definido en la clase
     $resultado = $curso->listarCursos();
 
     //Enviamos el resultado a la vista como un JSON
     echo json_encode($resultado);
+  }
+
+  if ($_POST['operacion'] =='buscarID'){
+
   }
 }
